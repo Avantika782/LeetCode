892ad4ce858f3d5ff1a91986e72c88dce069d93b
Avantika Singh <77248593+Avantika782@users.noreply.github.com>
2021-06-23 18:18:08 +0530

    Create 673_Number_of_Longest_Increasing_Subsequence.cpp

---
+++ 673_Number_of_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> lis(n,1), count(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(lis[i]<=lis[j]){
+                        lis[i]=lis[j]+1;
+                        count[i]=count[j];
+                    }else if(lis[i]==lis[j]+1){
+                        count[i]+=count[j];
+                    }
+                }
+            }
+        }
+        
+        int longest = *max_element(lis.begin(), lis.end());
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(lis[i]==longest) ans+=count[i];
+        }
+        
+        return ans;
+    }
+};
